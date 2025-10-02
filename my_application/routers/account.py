810ca56bd4405aab5ce17ee30aadd38e670cc47f
sean.blevins <sean.blevins@uni.lu>
2025-10-02 17:38:21 +0200

    Fix user account endpoints and move them around

--- my_application/routers/account.py
+++ my_application/routers/account.py
@@ -1,10 +1,26 @@
-from ninja import Router
+from django.contrib.auth.models import User
+from ninja import Router, ModelSchema
 from ninja.security import django_auth
 from ninja_jwt.authentication import JWTAuth
 
-router = Router(tags=["accounts"], auth=[JWTAuth(), django_auth])
+router = Router(tags=["accounts"])
 
-@router.get("/me")
+class UserOutSchema(ModelSchema):
+    class Meta:
+        model = User
+        fields = ["username", "is_staff", "is_active", "is_superuser"]
+
+@router.get("/me", response=UserOutSchema, auth=[JWTAuth(), django_auth])
 def me(request):
+    user = request.user
+    return user
+
+@router.get("/me_session_auth", response=UserOutSchema, auth=django_auth)
+def me_session_auth(request):
+    user = request.user
+    return user
+
+@router.get("/me_jwt_auth", response=UserOutSchema, auth=JWTAuth())
+def me_jwt_auth(request):
     user = request.user
     return user
