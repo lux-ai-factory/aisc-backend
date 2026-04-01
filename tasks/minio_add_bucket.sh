0d750b75193d96fd410806e70729d09d5d27662a
seanblevins <sean.blevins@uni.lu>
2026-04-01 14:39:11 +0200

    Add support for `artifacts` bucket in MinIO configuration and update related environment variables and Docker settings

--- tasks/minio_add_bucket.sh
+++ tasks/minio_add_bucket.sh
@@ -6,7 +6,8 @@
 # 1. Configure the MinIO client alias with credentials
 # 2. Create required buckets if they don't exist:
 #    - datasets: For storing dataset files
-#    - models: For storing model artifacts
+#    - models: For storing model files
+#    - artifacts: for storing evaluation output artifacts
 #
 # Environment variables required:
 # - MINIO_ROOT_USER: MinIO root access key
@@ -18,3 +19,4 @@ mc alias set minio http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 # Create required buckets (ignore if they already exist)
 mc mb --ignore-existing minio/datasets
 mc mb --ignore-existing minio/models
+mc mb --ignore-existing minio/artifacts
