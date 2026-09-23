f0de959bae14a1e9841ad17f10e0543efe483b6f
seanblevins <sean.blevins@uni.lu>
2026-09-23 15:03:17 +0200

    Simplify `get_components` and remove unused `get_ai_systems` method in project and evaluation models

--- aisc_backend/models/project.py
+++ aisc_backend/models/project.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from .ai_system import AIComponent
 from .project_config import ProjectConfig
 from .evaluation import Evaluation
 from .common import Base
@@ -17,15 +18,8 @@ class ProjectStatus(models.TextChoices):
 class Project(Base):
     status = models.CharField(max_length=255, choices=ProjectStatus.choices)
 
-    def get_components(self) -> list:
-        # Prefetched (via ProjectRepository) as self._aisystem -> AISystem.
-        system = getattr(self, "_aisystem", None)
-        if system is not None:
-            return list(system.components.all())
-        if self.aisystem_id is not None:
-            return list(self.aisystem.components.all())
-        from .ai_system import AIComponent
-        return list(AIComponent.objects.filter(system__project=self))
+    def get_components(self) -> list[AIComponent]:
+        return list(self.aisystem.components.all())
 
     def get_evaluations(self) -> list[Evaluation]:
         return list(self.evaluations.all())
